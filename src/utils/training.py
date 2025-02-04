0a59f4cbf7260067bd78e7fecbca0875ee97cbf9
Jesús Lozano <jesus.lozano@fundacionctic.org>
2025-02-04 11:15:25 +0100

    Update training.py

--- src/utils/training.py
+++ src/utils/training.py
@@ -3,22 +3,49 @@ import time
 import torch
 from sklearn.metrics import confusion_matrix, precision_score, recall_score, f1_score
 from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader
 from src.utils.plotting import confusion_matrix_plot
 
 class Trainer:
+    """
+    A versatile training pipeline for deep learning models, supporting classical, hybrid and pure quantum architectures.
+    Args:
+        model (torch.nn.Module): The neural network model to be trained.
+        train_loader (DataLoader): DataLoader for the training dataset.
+        val_loader (DataLoader): DataLoader for the validation dataset.
+        epochs (int): Number of training epochs.
+        log_wandb (bool): If True, log metrics to Weights & Biases.
+        wandb_project (str): W&B project name (used if log_wandb=True).
+        wandb_run_name (str): W&B run name (used if log_wandb=True).
+        use_quantum (bool): If True, enables quantum-based layers.
+        plot (bool): If True, generates and displays confusion matrix.
+        allowed_classes (list): List of class labels allowed for training and evaluation. If None, all classes are used.
+        lr (float): learning rate for the optimizer.
+    """
     def __init__(self,
+                model: torch.nn.Module,
+                train_loader: DataLoader,
+                val_loader: DataLoader,
+                epochs: int = 10,
                 log_wandb: bool = True,
                 wandb_project: str = 'Prueba train',
                 wandb_run_name: str = '1',
                 use_quantum: bool = True,
                 plot: bool = True,
-                allowed_class_idx = None,
+                allowed_classes: list = None,
+                lr: float = 0.01,
                 ):
 
+        self.model = model
+        self.train_loader = train_loader
+        self.val_loader = val_loader
+        self.epochs = epochs
         self.log_wandb = log_wandb
         self.wandb_project = wandb_project
         self.wandb_run_name = wandb_run_name
         self.use_quantum = use_quantum
+        self.plot = plot
+        self.lr = lr
         self.train_losses = []
         self.train_accuracies = []
         self.val_losses = []
@@ -30,30 +57,32 @@ class Trainer:
         self.plot = plot
         self.confusion_matrix_train = None
         self.confusion_matrix_val = None
-        
-        labels = ['Annual\nCrop', 'Forest',
-                  'Herbaceous\nVegetation',
+    
+        labels = ['AnnualCrop', 'Forest',
+                  'HerbaceousVegetation',
                   'Highway', 'Industrial',
-                  'Pasture', 'Permanent\nCrop',
+                  'Pasture', 'PermanentCrop',
                   'Residential', 'River',
                   'SeaLake']
+    
+        if allowed_classes is None:
+            self.labels = labels
+        else:
+            self.labels = []
+            for class_ in labels:
+                if class_ in allowed_classes:
+                    self.labels.append(class_)
 
-        self.labels = labels if allowed_class_idx is None else [labels[i] for i in allowed_class_idx if i < len(labels)]
     
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     
-    def fit(self, 
-            model,
-            train_loader: DataLoader,  
-            val_loader: DataLoader,
-            learning_rate: float,
-            epochs: int = 10,
-           ):
-        
-        model = model.to(self.device)
+    def fit(self):
+        model = self.model.to(self.device)
+        train_loader, val_loader = self.train_loader, self.val_loader
+        lr, epochs = self.lr, self.epochs
         
         criterion = self.criterion
-        optimizer = torch.optim.Adam(model.parameters(), lr = learning_rate)
+        optimizer = torch.optim.Adam(model.parameters(), lr = lr)
         
         all_labels = []
         all_preds = []        
@@ -61,7 +90,7 @@ class Trainer:
         if self.log_wandb:
             wandb.init(project = self.wandb_project, name = self.wandb_run_name)
             wandb.config.update({
-                "lr": learning_rate,
+                "lr": lr,
                 "epochs": epochs,
                 #"batch_size": self.batch_size,
                 #"qkernel_shape": self.qkernel_shape,
@@ -120,7 +149,10 @@ class Trainer:
                     "train_loss": train_loss,
                     "train_acc": train_acc,
                     "val_loss": val_loss,
-                    "val_acc": val_acc
+                    "val_acc": val_acc,
+                    "precision": precision,
+                    "recall": recall,
+                    "f1": f1
                 })
         
             print(f"Epoch [{epoch+1}/{epochs}]: "
@@ -139,6 +171,7 @@ class Trainer:
         """
         Evaluate the model on a given DataLoader. Returns average loss and accuracy.
         """
+        
         model.eval()
         val_loss, val_acc, running_loss, correct, total = 0.0, 0.0, 0.0, 0, 0
 
